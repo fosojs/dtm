7d913c397559933e52356eb0de7b7b9bfb606382
Zoltan Kochan <zoltan.kochan@gmail.com>
2015-11-21 15:51:53 +0200

    Styles loading implemented.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 'use strict';
 
-exports.Bundler = require('./lib/bundler');
 exports.PackagesLoader = require('./lib/packages-loader');
--- test/packages-loader.js
+++ test/packages-loader.js
@@ -8,7 +8,7 @@ var ung = window.ung = {
   origin: 'cdn.kochan.io'
 };
 
-describe('PackagesLoader', function() {
+describe('PackagesLoader for js', function() {
   it('should compose a package link', function() {
     var packageLoader = new PackagesLoader('js', {
       id: 'test-1'
@@ -47,6 +47,31 @@ describe('PackagesLoader', function() {
     var el = document.getElementById('test-2');
     expect(el.tagName).to.eq('SCRIPT');
     expect(el.src).to.eq('http://cdn.kochan.io/packages/foo@43.2.1!qar.js');
+    el.parentNode.removeChild(el);
+  });
+});
+
+describe('PackagesLoader for css', function() {
+  it('should compose a package link', function() {
+    var packageLoader = new PackagesLoader('css', {
+      id: 'test-1'
+    });
+    packageLoader.addPackage({
+      name: 'foo',
+      version: '1.3.1',
+      files: [
+        'lib/bar',
+        'foo'
+      ]
+    });
+    packageLoader.addPackage({
+      name: 'charlie',
+      version: '10.3.1'
+    });
+    packageLoader.write();
+    var el = document.getElementById('test-1');
+    expect(el.tagName).to.eq('LINK');
+    expect(el.href).to.eq('http://cdn.kochan.io/packages/foo@1.3.1!lib/bar;foo,charlie@10.3.1.css');
     el.parentNode.removeChild(el);
   });
 });
