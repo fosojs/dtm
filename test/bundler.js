c42ebb2aad522a41cce6bedb828a0bea53225372
Zoltan Kochan <zoltan.kochan@gmail.com>
2015-12-13 14:41:48 +0200

    Files from the same package are united.

--- test/bundler.js
+++ test/bundler.js
@@ -49,6 +49,34 @@ describe('Bundler for js', function() {
     expect(el.src).to.eq('http://cdn.kochan.io/bundle/foo@43.2.1(qar).js');
     el.parentNode.removeChild(el);
   });
+
+  it('should add several files when they are added with separate calls to the bundler API', function() {
+    var bundler = new Bundler('js', {
+      id: 'test-1'
+    });
+    bundler.addPackage({
+      name: 'foo',
+      version: '1.3.1',
+      files: [
+        'lib/bar'
+      ]
+    });
+    bundler.addPackage({
+      name: 'foo',
+      files: [
+        'foo'
+      ]
+    });
+    bundler.addPackage({
+      name: 'charlie',
+      version: '10.3.1'
+    });
+    bundler.write();
+    var el = document.getElementById('test-1');
+    expect(el.tagName).to.eq('SCRIPT');
+    expect(el.src).to.eq('http://cdn.kochan.io/bundle/foo@1.3.1(lib/bar+foo),charlie@10.3.1.js');
+    el.parentNode.removeChild(el);
+  });
 });
 
 describe('Bundler for css', function() {
